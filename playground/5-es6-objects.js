a96369a873a8e9d6ba3e92491eb2ebf0de4f02cb
Peter Bakshaev <bak.pet@gmail.com>
2022-07-08 17:32:12 -0700

    js object

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -7,7 +7,14 @@ const user = {
 
 console.log(user);
 
-const { firstName, location, address } = user;
+const { firstName, location, address, rating = 5 } = user;
 console.log(firstName);
 console.log(location);
 console.log(address);
+console.log(rating);
+
+const transcation = (type, { firstName, age }) => {
+  console.log(type, firstName, age);
+};
+
+transcation('Person', user);
